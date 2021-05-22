070ea52dbf8d00a322a7800c614b3bcab38104d2
student <audreymunyiva@gmail.com>
2021-05-22 08:49:13 +0300

    first commit

---
+++ game.py
@@ -0,0 +1,31 @@
+print("Simple math trivia")
+
+print("What is 12+2")
+
+Answer= input("Your answer is: ")
+if Answer==14:
+    print("Good Job")
+else:
+    print("Game Over..")
+
+
+print("What is 2*2")
+answer1=input("Your answer is: ")
+if answer1==4:
+    print("Good Job!")
+else:
+    print("Game Over..")
+
+
+print("What is 2+8")
+Ans=input("Your answer is: ")
+if Ans == 10:
+    print("You win!")
+else:
+    print("Game Over ..")
+
+
+
+
+
+
